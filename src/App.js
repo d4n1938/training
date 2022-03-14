b5487e76bc28936b35cce89a426b3c2347c11d87
d4n1938 <daniksb@icloud.com>
2022-03-14 18:18:57 +0900

    animation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import { useTransition, animated } from "react-spring";
 
 function App() {
+  const [isVisivle, setIsVisible] = useState(false);
+  const transition = useTransition(isVisivle, {
+    from: { x: -100, y: 800, opacity: 0 },
+    enter: { x: 0, y: 0, opacity: 1 },
+    leave: { x: 100, y: 800, opacity: 0 },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      <button
+        onClick={() => {
+          setIsVisible((v) => !v);
+        }}
+      >
+        {isVisivle ? <div>un-mount</div> : <div>mount</div>}
+      </button>
+      <div className="container">
+        {transition((style, item) =>
+          item ? <animated.div style={style} className="item" /> : ""
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/components/StartMenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled/components";
+
+const Button = styled.button({
+  margin: "20px",
+});
+export const StartMenu = () => {
+  return (
+    <div>
+      StartMenu
+      <Button>aaaa</Button>
+    </div>
+  );
+};
